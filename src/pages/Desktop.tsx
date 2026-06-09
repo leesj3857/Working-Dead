0dc82437159cd62e67b7902e996062817a3cf565
leesj3857 <hensin12@gmail.com>
2026-06-09 22:29:55 +0900

    안쓰는 prop 제거

--- src/pages/Desktop.tsx
+++ src/pages/Desktop.tsx
@@ -1,24 +1,15 @@
-import { useState } from 'react'
 import MobileOnlyModal from '../components/MobileOnlyModal/MobileOnlyModal'
 
 export default function Desktop() {
-  const [closeFailed, setCloseFailed] = useState(false)
-
   const handleClose = () => {
     window.open('', '_self')
     window.close()
-    setTimeout(() => {
-      if (!window.closed) {
-        setCloseFailed(true)
-      }
-    }, 100)
   }
 
   return (
     <MobileOnlyModal
       onClose={handleClose}
       onConfirm={handleClose}
-      closeFailed={closeFailed}
     />
   )
 }
